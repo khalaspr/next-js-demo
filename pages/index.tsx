2892ad5bec58baccef5f4e4ce59ec6687297b9e6
pk2616 <pk2616@att.com>
2021-10-28 00:39:35 +0530

    added files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, GetStaticPropsResult, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Character, GetCharactersResults} from '../types'
 
-const Home: NextPage = () => {
+
+export const getStaticProps: GetStaticProps =async(context)=> {
+
+  const res = await fetch(`https://rickandmortyapi.com/api/character`)
+  const data : GetCharactersResults = await res.json()
+  return {
+    props: { 
+      characters:data.results
+     }, // will be passed to the page component as props
+  }
+}
+
+const Home: NextPage<{characters: Character[]}> = ({ characters } ) => {
   return (
+    <>
+ 
+   
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -13,59 +29,40 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+       
+      <h1>Character Details</h1>
+      <ul className="character-info">
+   {
+         characters.map((c: Character)=>(
+          <li key={c.id}> 
+          
+          <div className="box">
+          <div>
+          <img src={c.image}  width="100" height="100" />
+            </div>
+            <div className="char-info">
+            <span>{c.name} </span>
+            <span>{c.gender}</span>
+            <span>{c.location.name}</span>
+            <span>{c.status}</span>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+              </div>
+            </div>
+          
+          
+          
+          </li>
+        ))
+    }
+   </ul>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+       This is footer
       </footer>
     </div>
+
+    </>
   )
 }
 
---
+++ types.ts
@@ -0,0 +1,29 @@
+export interface GetCharactersResults {
+    info: Info;
+    results?: (Character)[] | null;
+  }
+  export interface Info {
+    count: number;
+    pages: number;
+    next: string;
+    prev?: null;
+  }
+  export interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: OriginOrLocation;
+    location: OriginOrLocation;
+    image: string;
+    episode?: (string)[] | null;
+    url: string;
+    created: string;
+  }
+  export interface OriginOrLocation {
+    name: string;
+    url: string;
+  }
+  
